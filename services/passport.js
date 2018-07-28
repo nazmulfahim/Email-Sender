1962ce9a9809a375badd60089b655219bcaa9237
unknown <bestwebmaster75@gmail.com>
2018-07-28 19:11:29 +0600

    Added billing and client app

--- services/passport.js
+++ services/passport.js
@@ -19,12 +19,16 @@ clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback',
     proxy: true
-},(accessToken, refreshToken, profile, done) => {
-    User.findOne({ googleId: profile.id}).then(existingUser => {
-      if(existingUser){done( null, existingUser,);
+}, 
+   async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await  User.findOne({ googleId: profile.id})
+      if(existingUser){
+        return done( null, existingUser,);
       }
-       else {  new User ({ googleId: profile.id }).save().then(user => done(null, user));
-       }
-     });
+        const user = await new User ({ googleId: profile.id }).save()
+        done(null, user);
    }
-));
+  )
+);
+
+
